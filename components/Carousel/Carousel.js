b82cb44adc56b95ab51a52a1cdaad58cd49c5c11
Eian Carter <eian@bricleir.com>
2019-12-06 11:27:27 -0800

    added carousel stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,39 @@
     <div class="right-button"> > </div>
   </div>
 */
+imageList = ['/Users/eiancarter/Desktop/LambdaProjects/Sprint-Challenge-Applied-Javascript/assets/bones.jpg','assets/fido.jpg','assets/max.jpg','assets/puppers.jpg']
+
+const carouselData = document.querySelector('.carousel-container');
+carouselData.appendChild(createCarousel(imageList));
+
+function createCarousel (array) {
+    //variables
+      const carousel = document.createElement('div');
+      const leftButton  = document.createElement('div');
+      const imgSource1 = document.createElement('img');
+      const imgSource2 = document.createElement('img');
+      const imgSource3 = document.createElement('img');
+      const imgSource4 = document.createElement('img');
+      const rightButton = document.createElement('div');
+    //append children
+      carousel.appendChild(leftButton);
+      carousel.appendChild(imgSource1);
+      carousel.appendChild(imgSource2);
+      carousel.appendChild(imgSource3);
+      carousel.appendChild(imgSource4);
+      carousel.appendChild(rightButton);
+    //add classes
+      card.classList.add('carousel');
+      leftButton.classList.add('left-button');
+      rightButton.classList.add('right-button');
+    //text content
+      leftButton.textContent = 'Left';
+      rightButton.src = 'Right';
+      imgSource1.src = array[0];
+      imgSource2.src = array[1];
+      imgSource3.src = array[2];
+      imgSource4.src = array[3];
+ 
+    
+  return carousel
+}
